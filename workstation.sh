47f165252c6e0648cec6e03f8aa3e8b0ecdcee15
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-05-19 18:54:43 +0530

    new eks yaml

--- workstation.sh
+++ workstation.sh
@@ -68,5 +68,7 @@ VALIDATE $? "kubectl installation"
 
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+
 sudo mv /tmp/eksctl /usr/local/bin
 
